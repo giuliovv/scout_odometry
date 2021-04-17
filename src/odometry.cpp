b57405cc1c8769c6dbb85264cd558f152d83f174
Giulio Vaccari <giulio09@marcov.it>
2021-04-18 01:21:28 +0200

    Odometry

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,16 +1,32 @@
 #include "ros/ros.h"
 #include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
 #include "robotics_first/MotorSpeed.h"
 
+void callback(const robotics_first::MotorSpeedConstPtr& msg1, 
+              const robotics_first::MotorSpeedConstPtr& msg2,
+              const ros::Publisher odometry) {
+
+    ROS_INFO ("Received two messages: (%f,%f)", 
+        msg1->rpm, msg2->rpm);
+    odometry.publish(msg1);
+}
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "odometry");
+    ros::init(argc, argv, "syncronizer");
 
     ros::NodeHandle n;
 
-    message_filters::Subscriber<robotics_first::MotorSpeed> sub1(n, "sync_left", 1);
-    message_filters::Subscriber<robotics_first::MotorSpeed> sub2(n, "sync_right", 1);
+    ros::Publisher odometry = n.advertise<robotics_first::MotorSpeed>("odometry", 1000);
+
+    message_filters::Subscriber<robotics_first::MotorSpeed> sub1(n, "motor_speed_fl", 1);
+    message_filters::Subscriber<robotics_first::MotorSpeed> sub2(n, "motor_speed_fr", 1);
+    // message_filters::Subscriber<robotics_first::MotorSpeed> sub3(n, "motr_speed_rl", 1);
+    // message_filters::Subscriber<robotics_first::MotorSpeed> sub4(n, "motor_speed_rr", 1);
+    message_filters::TimeSynchronizer<robotics_first::MotorSpeed, 
+                                        robotics_first::MotorSpeed> sync(sub1, sub2, 10);
+    sync.registerCallback(boost::bind(&callback, _1, _2, odometry));
 
     ros::spin();
 
