a2b38a84494bbb4c1f0ae88fe35ca88b8785056e
Giulio Vaccari <giulio09@marcov.it>
2021-04-23 00:27:32 +0200

    Clean up pt 2

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -37,6 +37,9 @@ class Odometry{
             odometry = n.advertise<nav_msgs::Odometry>("odometry", 1000);
             custom_odometry = n.advertise<robotics_first::CustomOdometry>("custom_odometry", 1000);
 
+            reset_odometry = n.advertiseService("reset_odometry", &Odometry::resetOdometry, this);
+            reset_odometry_to_pose = n.advertiseService("reset_to_pose", &Odometry::resetToPose, this);
+
         }
 
     
@@ -131,6 +134,7 @@ class Odometry{
     ros::Publisher odometry;
     ros::Publisher custom_odometry;
     ros::Subscriber sub;
+    ros::ServiceServer reset_odometry;
     ros::ServiceServer reset_odometry_to_pose;
     std_msgs::String method_for_custom_odom;
     nav_msgs::Odometry odo_msg;
