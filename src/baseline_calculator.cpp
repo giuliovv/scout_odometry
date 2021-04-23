b109ac66eeb5107a2298a14ab905667d647c8097
Giulio Vaccari <giulio09@marcov.it>
2021-04-23 13:19:20 +0200

    Baseline calculator ready to use

--- src/baseline_calculator.cpp
+++ src/baseline_calculator.cpp
@@ -10,68 +10,77 @@
 #include "robotics_first/MotorSpeed.h"
 
 #define R 0.1575
-#define GEAR_RATIO 38
+#define ALPHA 0.1
 
-// float old = 0;
-// int oldtempo = 1;
-
-void callback(const robotics_first::MotorSpeedConstPtr& left, 
-              const robotics_first::MotorSpeedConstPtr& right,
+void callback(const robotics_first::MotorSpeedConstPtr& rear_left, 
+              const robotics_first::MotorSpeedConstPtr& rear_right,
+              const robotics_first::MotorSpeedConstPtr& front_left, 
+              const robotics_first::MotorSpeedConstPtr& front_right,
               const nav_msgs::OdometryConstPtr& odo,
-              const ros::Publisher appa_baseline) {
+              float& sum_of_gear_ratios,
+              int& iteration_number,
+              float& ema_apparent_baselines,
+              int& apparent_baseline_iteration_number) {
 
-    // ROS_INFO("Left RPM: %f", left -> rpm);
+    float v_left_no_gear_ratio = (rear_left->rpm + front_left->rpm)/2 * 2 * M_PI * R / 60;
+    float v_right_no_gear_ratio = (rear_right->rpm + front_right->rpm)/2 * 2 * M_PI * R / 60;
 
-    float v_left = (left->rpm) * 2 * M_PI * R / (60 * GEAR_RATIO);
-    float v_right = (right->rpm) * 2 * M_PI * R / (60 * GEAR_RATIO);
-
-    float v_x = (- v_left + v_right)/2;
+    float v_x_no_gear_ratio = (- v_left_no_gear_ratio + v_right_no_gear_ratio)/2;
 
     float w = odo->twist.twist.angular.z;
-    std_msgs::Float64 appa;
-    appa.data = (v_left+v_right)/w;
-
     float v_x_read = odo->twist.twist.linear.x;
 
-    // float v_x_ground = (scout -> pose.position.x - old)/(scout -> header.stamp.sec - oldtempo);
+    float gear_ratio = v_x_no_gear_ratio/v_x_read;
 
-    if (v_left/v_right < 1.2 && v_left/v_right > 0.8 && v_left * v_right > 0){
-        ROS_INFO("Vl: %f Vr: %f, w: %f, base: %f", v_left, v_right, w, appa.data);
+    if (v_x_read != 0){
+        sum_of_gear_ratios += gear_ratio;
+        iteration_number +=1;
+
+        float mean_gear_ratio = sum_of_gear_ratios / iteration_number;
+
+        ROS_INFO("Gear ratio: %f", mean_gear_ratio);
+
+        float v_left = v_left_no_gear_ratio / mean_gear_ratio;
+        float v_right = v_right_no_gear_ratio / mean_gear_ratio;
+        double apparent_baseline;
+        apparent_baseline = (v_left+v_right)/w;
+
+        if (v_left/v_right < 1.2 && v_left/v_right > 0.8 && v_left * v_right > 0){
+            // Exponential moving average
+            ema_apparent_baselines = (ALPHA * apparent_baseline) + (1.0 - ALPHA) * ema_apparent_baselines;
+            apparent_baseline_iteration_number += 1;
+            ROS_INFO("Vl: %f Vr: %f, w: %f, mean apparent baseline: %f", v_left, v_right, w, ema_apparent_baselines);
+        }
     }
 
-    // ROS_INFO("APPARENT: (%f, %f)", w, appa.data);
-
-    // ROS_INFO("Vx: (%f, %f)", v_x, v_x_read);
-    // ROS_INFO("Gear ratio: %f", v_x/v_x_read);
-
-    // ROS_INFO("Vl: %f Vr: %f, w: %f, base: %f", v_left, v_right, w, appa.data);
-
-    // ROS_INFO("tempo %f", scout->pose.position.x);
-
-    // old = scout -> pose.position.x;
-    // oldtempo = scout -> header.stamp.sec;
-
-    appa_baseline.publish(appa);
 }
 
 int main(int argc, char** argv) {
+
+    float sum_of_gear_ratios = 0;
+    int iteration_number = 0;
+
+    float ema_apparent_baselines = 0;
+    int apparent_baseline_iteration_number = 0;
+
     ros::init(argc, argv, "baseline_calculator");
 
     ros::NodeHandle n;
 
-    ros::Publisher appa_baseline = n.advertise<std_msgs::Float64>("apparent_baseline", 1000);
-
-    message_filters::Subscriber<robotics_first::MotorSpeed> sub1(n, "motor_speed_rl", 1);
-    message_filters::Subscriber<robotics_first::MotorSpeed> sub2(n, "motor_speed_rr", 1);
-    message_filters::Subscriber<nav_msgs::Odometry> sub3(n, "scout_odom", 1);
-    // message_filters::Subscriber<geometry_msgs::PoseStamped> sub4(n, "gt_pose", 1);
-    // message_filters::Subscriber<robotics_first::MotorSpeed> sub4(n, "motor_speed_rr", 1);
+    message_filters::Subscriber<robotics_first::MotorSpeed> rl(n, "motor_speed_rl", 1);
+    message_filters::Subscriber<robotics_first::MotorSpeed> rr(n, "motor_speed_rr", 1);
+    message_filters::Subscriber<robotics_first::MotorSpeed> fl(n, "motor_speed_fl", 1);
+    message_filters::Subscriber<robotics_first::MotorSpeed> fr(n, "motor_speed_fr", 1);
+    message_filters::Subscriber<nav_msgs::Odometry> odom(n, "scout_odom", 1);
     message_filters::TimeSynchronizer<robotics_first::MotorSpeed, 
                                         robotics_first::MotorSpeed,
+                                        robotics_first::MotorSpeed,
+                                        robotics_first::MotorSpeed,
                                         nav_msgs::Odometry
-                                        // geometry_msgs::PoseStamped
-                                        > sync(sub1, sub2, sub3, 10);
-    sync.registerCallback(boost::bind(&callback, _1, _2, _3, appa_baseline));
+                                        > sync(rl, rr, fl, fr, odom, 10);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4, _5,
+                                         sum_of_gear_ratios, iteration_number,
+                                         ema_apparent_baselines, apparent_baseline_iteration_number));
 
     ros::spin();
 
